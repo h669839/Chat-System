d61f9635c464784bbbb34afdd9c552ef2be160bd
Eivind Hustvedt <669839@stud.hvl.no>
2024-10-08 19:58:07 +1000

    Added Login error handling

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -24,6 +24,7 @@ export class LoginComponent {
   @Output() loginSuccess: EventEmitter<void> = new EventEmitter<void>();
   username: string = '';
   password: string = '';
+  errorMessage: string = '';
 
   constructor(
     private http: HttpClient,
@@ -34,32 +35,34 @@ export class LoginComponent {
   //Checks the if the login credentials are correct, and saves the user to localStorage.
   //It also redirects the user depending on their role.
   login() {
-    console.log('Login button clicked');
     let user = {username:this.username, password:this.password};
-    console.log('User data:', user);
-    
+
     this.http.post('http://localhost:3000/login', user, httpOptions)
       .subscribe({ 
         next: (response: any) => {  
-          console.log("Responsive received:", response);
         if (response.ok) {
           // Store user info in localStorage
           localStorage.setItem('user', JSON.stringify(response));
           this.appComponent.checkUserStatus(); // Call method in AppComponent to update state
           this.cdr.detectChanges(); // Trigger change detection manually
           // Redirect based on role
-          const roles = response.roles;
-          if (roles.includes('Super Admin')) {
-            this.router.navigate(['/admin']);
-          } else if (roles.includes('Group Admin')) {
-            this.router.navigate(['/group']);
-          } else {
-            this.router.navigate(['/chat']);
-          }
-        } else {
-          this.router.navigate(['/login']);
+          this.router.navigate(['/user-dashboard']);
+         
         }
-      }
+      }, error: (err) => {
+        if (err.status === 401) {
+          this.errorMessage = 'Invalid username or password. Please try again.';
+          this.resetForm();
+        } else {
+          this.errorMessage = 'An error occurred. Please try again later.';
+        }
+      } 
     });
+  }
+
+  // Method to reset the input fields
+  resetForm() {
+    this.username = '';
+    this.password = '';
   }
 }
